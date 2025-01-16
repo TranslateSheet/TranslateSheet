51fcb638d819146cf76c080e4aee3ba97856fffe
Brandon Austin <61210295+branaust@users.noreply.github.com>
2025-01-16 11:47:33 -0800

    init

--- dist/cli/index.mjs
+++ dist/cli/index.mjs
@@ -213,7 +213,7 @@ var init_translateContent = __esm({
       try {
         console.log("Sending translation request...");
         const response = yield fetch(
-          "https://api.translatesheet.co/api/translations",
+          "https://api.translatesheet.co/translateContent",
           {
             method: "POST",
             headers: {
