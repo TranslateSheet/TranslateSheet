7314f9b406263880c1ee64d87e0606a0f93a027d
Brandon Austin <61210295+branaust@users.noreply.github.com>
2025-03-11 18:04:37 -0700

    common namespaces

--- src/helpers/formatAsJSON.ts
+++ src/helpers/formatAsJSON.ts
@@ -1,5 +1,6 @@
 /**
  * Generate a JSON string with properly formatted content.
+ * making a small change
  */
 const formatAsJSON = (content: Record<string, any>): string => {
   return JSON.stringify(content, null, 2);
