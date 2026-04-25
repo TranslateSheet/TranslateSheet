9674e688fda1a7e4931e3793db1ba91933b587c4
brandon <baustin@taekus.com>
2026-04-25 12:28:02 -0300

    updates

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -9,4 +9,22 @@ declare const TranslateSheet: {
 
 declare const useLanguageChange: () => void;
 
-export { TranslateSheet as default, useLanguageChange };
+type FileExtensions = ".ts" | ".js" | ".json";
+interface TranslateSheetConfig {
+    /** The API key used for AI-powered translations */
+    apiKey: string;
+    /** The id used to associate the project */
+    projectId?: string;
+    /** The primary language of the project (e.g., "en" for English) */
+    primaryLanguage: string;
+    /** An array of target languages for translation (e.g., ["es", "fr", "de"]) */
+    languages: string[];
+    /** The file extension for the generated translation files */
+    fileExtension: FileExtensions;
+    /** The output directory where the generated translation files will be saved */
+    output: string;
+    /** The output directory where the generated translation files will be saved */
+    generatePrimaryLanguageFile: string | boolean;
+}
+
+export { type FileExtensions, type TranslateSheetConfig, TranslateSheet as default, useLanguageChange };
